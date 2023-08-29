3bd4b9d9684646ace20fdf4535b1448c53092971
Renzo Fattorini <renzofatto@gmail.com>
2023-08-29 19:02:06 -0300

    color black for sections

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -6,7 +6,7 @@ function AboutMe() {
       title='About Me'
       subtitle='Im Renzo Fattorini'
       color="white"
-      backgroundColor='midnight'
+      backgroundColor='black'
       footerContent={
         <div slot="footer">
           <a className="border-[2px] border-white bg-white/5 backdrop-blur-xl text-sm rounded font-medium text-white px-12 py-2 inline-block hover:bg-white hover:text-black transition-colors" href="#">
